bacef048d274f3f7bec63e5490e379fb8f39f4ec
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-11 20:21:12 +0100

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,11 +2,13 @@
 """Definition of filter_datum function"""
 import logging
 import re
-from typing import List
+from typing import Sequence
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str
+        fields: Sequence, redaction: str, message: str, separator: str
 ) -> str:
     """Replace occurrence of certain field value"""
     for field in fields:
@@ -23,7 +25,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: Sequence):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
@@ -32,3 +34,17 @@ class RedactingFormatter(logging.Formatter):
         log_msg: str = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             log_msg, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """Return logger instance"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    logger.addHandler(handler)
+
+    return logger
