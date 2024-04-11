bde01c4f43f1570bc7eaa43916f316ca735a46b4
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-11 11:27:33 +0100

    fix: reduce the function's length

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,21 +7,8 @@ from typing import List
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
-    """Replace occurrence of certain field value
-
-    Args:
-        fields (List[str]): A list of strings for all fields to obfuscate
-        redaction (str): A string to obfuscate the field with
-        message (str): A string representing the log line
-        separator (str): A string that separate all the fields
-                         in the log line (message)
-
-    Returns:
-        str: The log message obfuscated
-    """
+    """Replace occurrence of certain field value"""
     for field in fields:
-        if field in message:
-            obfuscated_msg = re.sub(fr"({field}=[^{separator}]+)",
-                                    f'{field}={redaction}', message)
-            message = obfuscated_msg
+        message = re.sub(fr"({field}=[^{separator}]+)",
+                         f'{field}={redaction}', message)
     return message
