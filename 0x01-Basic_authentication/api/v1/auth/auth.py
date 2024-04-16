68622999449ed09bf9fb5d49e22ca2c633cbcd15
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-17 00:36:09 +0100

    Allow excluded_path with "*"

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,14 +6,20 @@ from typing import List, TypeVar
 
 class Auth:
     """Authentication class"""
-    def require_auth(self, path: str, excluded_path: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determine the routes that require authentication
         """
-        if not path or not excluded_path:
+        if not path or not excluded_paths:
             return True
 
         path = path if path.endswith('/') else path + '/'
-        return path not in excluded_path
+        # Consider when excluded_paths = ["/api/v1/stat*"]
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                excluded_path = excluded_path[:-1]
+                if excluded_path in path:
+                    return False
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Get authorization
