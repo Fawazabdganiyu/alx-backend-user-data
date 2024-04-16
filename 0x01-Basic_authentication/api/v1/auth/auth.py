4091a8ae39c7e149640749d352621b2fad281918
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-16 12:11:41 +0100

    Update require_auth function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 """API Authentication module"""
 from flask import request
-from typing import List
+from typing import List, TypeVar
 
 
 class Auth:
     """Authentication class"""
-    def required_auth(self, path: str, excluded_path: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_path: List[str]) -> bool:
         """Determine the routes that require authentication
         """
-        return False
+        if not path or not excluded_path:
+            return True
+
+        path = path if path.endswith('/') else path + '/'
+        return path not in excluded_path
 
     def authorization_header(self, request=None) -> str:
         """Get authorization
