70c2f3fb84371015a4301dfbc3b72c3dc8566d37
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-23 23:04:15 +0100

    Implement login route with session_id generation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask app module"""
-from flask import Flask, jsonify, request, Response, abort
+from flask import Flask, jsonify, request, Response, abort, make_response
 
 from auth import Auth
 
@@ -33,6 +33,27 @@ def users():
         return jsonify({'email': email, 'message': 'user created'})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Login a user
+
+    form data:
+        - email
+        - password
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        abort(400, "Email or password is missing")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    resp = make_response(jsonify({"email": email, "message": "logged in"}))
+    resp.set_cookie('session_id', session_id)
+    return resp
 
 
 if __name__ == '__main__':
