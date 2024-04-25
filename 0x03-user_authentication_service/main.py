421ec1f4f86f260643a1342c4d2315dc70418b2d
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-25 22:18:23 +0100

    Complete end-to-end integration test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -8,6 +8,7 @@ def register_user(email: str, password: str) -> None:
     data = {'email': email, 'password': password}
     resp = requests.post('http://127.0.0.1:5000/users', data=data)
 
+    assert resp.status_code == 200
     assert resp.json() == {'email': email, 'message': 'user created'}
 
 
@@ -15,6 +16,7 @@ def log_in_wrong_password(email: str, password: str) -> None:
     """Test user login with wrong password"""
     data = {'email': email, 'password': password}
     resp = requests.post('http://127.0.0.1:5000/sessions', data=data)
+
     assert resp.status_code == 401
 
 
@@ -22,32 +24,57 @@ def log_in(email: str, password: str) -> str:
     """Test user normal login"""
     data = {'email': email, 'password': password}
     resp = requests.post('http://127.0.0.1:5000/sessions', data=data)
+
+    assert resp.status_code == 200
+    assert resp.json() == {"email": email, "message": "logged in"}
+
     return resp.cookies.get('session_id')
 
 
 def profile_unlogged() -> None:
     """Test user profile with unlogged user"""
-    pass
+    resp = requests.get('http://127.0.0.1:5000/profile')
+
+    assert resp.status_code == 403
 
 
 def profile_logged(session_id: str) -> None:
     """Test user profile with logged in user"""
-    pass
+    cookies = {'session_id': session_id}
+    resp = requests.get('http://127.0.0.1:5000/profile', cookies=cookies)
+
+    assert resp.status_code == 200
+    assert resp.json() == {"email": EMAIL}
 
 
 def log_out(session_id: str) -> None:
     """Test a user is logged out"""
-    pass
+    cookies = {'session_id': session_id}
+    resp = requests.delete('http://127.0.0.1:5000/sessions', cookies=cookies)
+
+    assert resp.status_code == 200
+    assert resp.json() == {"message": "Bienvenue"}
 
 
 def reset_password_token(email: str) -> str:
     """Test user reset password token"""
-    pass
+    resp = requests.post('http://127.0.0.1:5000/reset_password',
+                         data={'email': email})
+
+    assert resp.status_code == 200
+    assert email in resp.json().values()
+
+    return resp.json().get('reset_token')
 
 
 def update_password(email: str, reset_token: str, new_password: str) -> None:
     """Test user password update"""
-    pass
+    data = {'email': email, 'reset_token': reset_token,
+            'new_password': new_password}
+    resp = requests.put('http://127.0.0.1:5000/reset_password', data=data)
+
+    assert resp.status_code == 200
+    assert resp.json() == {'email': email, "message": "Password updated"}
 
 
 EMAIL = "guillaume@holberton.io"
